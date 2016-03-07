937a640f6cd90e715f7698773f27108003d070d1
wangoasis <wangqingsky@outlook.com>
2016-03-07 11:56:30 +0800

    add sPlayTree in chapter 12, c language

--- chapter_12/sPlayTree.h
+++ chapter_12/sPlayTree.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 typedef int elementType;
 #define infinity (10000)
 #define negInfinity (-10000)
@@ -20,26 +19,3 @@ sPlayTree delete(elementType, sPlayTree);
 void inOrder(sPlayTree);
 
 #endif
-=======
-typedef int elementType;
-#define infinity (10000)
-#define negInfinity (-10000)
-
-#ifndef _SPLAYTREE_H
-#define _SPLAYTREE_H
-
-struct sPlayNode;
-typedef struct sPlayNode *sPlayTree;
-
-sPlayTree init(void);
-sPlayTree makeEmpty(sPlayTree);
-sPlayTree find(elementType, sPlayTree);
-sPlayTree findMax(sPlayTree);
-sPlayTree findMin(sPlayTree);
-elementType retrieve(sPlayTree);
-sPlayTree insert(elementType, sPlayTree);
-sPlayTree delete(elementType, sPlayTree);
-void inOrder(sPlayTree);
-
-#endif
->>>>>>> origin/master
